b7cb5b9e622899aae3b4a21aebd4348907b55dff
Giovanni <giovanni.albano1995@libero.it>
2022-02-10 17:20:52 +0100

    #010 Reengineered creation form with formik, material ui and yup

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,43 +1,117 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Grid,
+    TextField,
+    MenuItem,
+    Slide,
+    Fab,
+    Button,
+    IconButton
+} from "@mui/material";
+
+import AddIcon from '@mui/icons-material/Add';
+import CloseIcon from '@mui/icons-material/Close';
+
+import itLocale from "date-fns/locale/it";
+
+import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import DatePicker from '@mui/lab/DatePicker';
+
+import { TransitionProps } from '@mui/material/transitions';
+
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+
 
 import './Main.scss';
 
 
 
+interface Simpson {
+    firstname: string,
+    lastname: string,
+    email: string,
+    birthdate: Date | null,
+    gender: string
+}
 interface MainProps {
     mainClass?: string,
     id?: string
 }
 
+
+const Transition = React.forwardRef(function Transition(
+    props: TransitionProps & {
+        children: React.ReactElement<any, any>;
+    },
+    ref: React.Ref<unknown>,
+) {
+    return <Slide direction="up" ref={ref} {...props} />;
+});
+
+
 const Main = (props: MainProps) => {
 
     const firstTitleCard: string = 'Simpson family members:';
-    const secondTitleCard: string = 'Total members'
+    const secondTitleCard: string = 'Total members';
+
+    const initialFormState: Simpson = {
+        firstname: '',
+        lastname: '',
+        email: '',
+        birthdate: new Date(),
+        gender: ''
+    };
+
     const [familyMembers, setFamilyMembers] = useState<number>(0);
     const [members, setMembers] = useState<number>(0);
-    const [formData, setFormData] = useState<Object>({});
-
-    function closeModal(): void {
-        const element: HTMLFormElement = document.getElementById('modal-details') as HTMLFormElement;
-        return element.reset();
+    const [date, setDate] = useState<Date | null>(initialFormState.birthdate);
+    const [localeDate, setLocaleDate] = useState<{ locale: Locale, mask: string }>({ locale: itLocale, mask: '__/__/____' })
+    const [openModal, setOpenModal] = useState<boolean>(false);
 
 
 
-    }
+    useEffect(() => {
 
-    function handleInputChange(event: React.ChangeEvent<HTMLSelectElement | HTMLInputElement>): void {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value
-        });
-    }
+    }, [openModal])
 
-    function handleFormSubmit(e: React.FormEvent): void {
-        e.preventDefault();
-        console.log(e);
-    }
+
+    function handleOpenModal(): void {
+        setOpenModal(true);
+    };
+
+    function handleCloseModal(): void {
+        setOpenModal(false);
+        formik.resetForm();
+    };
+
+
+    const formik = useFormik({
+        initialValues: {
+            firstname: '',
+            lastname: '',
+            email: '',
+            birthdate: new Date(),
+            gender: ''
+        },
+        validationSchema: Yup.object({
+            firstname: Yup.string().required('Please enter your name'),
+            lastname: Yup.string().required('Please enter your lastname'),
+            email: Yup.string().email('Please enter your email address in format: yourname@example.com').required('Please enter your email'),
+            birthdate: Yup.date(),
+            gender: Yup.string().required('Please enter your gender'),
+        }),
+        onSubmit: values => {
+            console.log('Form data', values);
+        },
+    });
+
 
 
     function renderLeftMainColumn(): JSX.Element {
@@ -83,68 +157,116 @@ const Main = (props: MainProps) => {
 
     function renderFloatingButton(): JSX.Element {
         return (
-            <button type="button" className="btn btn-primary btn-lg rounded-circle new-button position-fixed" data-toggle="modal" data-target="#formDialog"><FontAwesomeIcon icon={['fas', 'plus']} /></button>
+            <Fab color="primary" aria-label="add" className="new-button position-fixed" onClick={handleOpenModal}>
+                <AddIcon />
+            </Fab>
         );
     }
 
-    function renderFormDialog(): JSX.Element {
+    function renderDialogHeader(): JSX.Element {
         return (
-            <div className="modal fade" id="formDialog" tabIndex={-1} role="dialog" aria-labelledby="registrationFormDialog" aria-hidden="true">
-                <div className="modal-dialog modal-dialog-centered" role="dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="registrationFormDialog">Member Registration</h5>
-                            <button type="button" className="btn-close" data-dismiss="modal" aria-label="Close" />
-                        </div>
-                        <div className="modal-body">
-                            <form id="modal-details" onSubmit={handleFormSubmit}>
-                                <div className="form-row mb-1">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="firstNameInput">Firstname</label>
-                                        <input type="text" className="form-control" id="firstNameInput" name="firstname" onChange={handleInputChange} />
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="lastNameInput">Lastname</label>
-                                        <input type="text" className="form-control" id="lastNameInput" name="lastname"  onChange={handleInputChange} />
-                                    </div>
-                                </div>
-                                <div className="form-row mb-1">
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="emailInput">Email</label>
-                                        <input type="email" className="form-control" id="emailInput" name="email" onChange={handleInputChange} />
-                                    </div>
-                                    <div className="form-group col-md-6">
-                                        <label htmlFor="birthDateInput">Date of Birth</label>
-                                        <input type="date" className="form-control" id="birthDateInput" date-date-format="dd/mm/yyyy" name="date" onChange={handleInputChange} />
-                                    </div>
-                                </div>
-                                <div className="form-row mb-1"> <div className="form-group col-md-6">
-                                    <label htmlFor="genderInput">Gender</label>
-                                    <select className="form-control custom-select" id="genderInput" name="gender" onChange={handleInputChange} >
-                                        <option defaultValue={'default'}>Choose a gender</option>
-                                        <option value={'male'}>Male</option>
-                                        <option value={'female'}>Female</option>
-                                        <option value={'other'}>Other</option>
-                                    </select>
-                                </div>
-                                </div>
-                            </form>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal" onClick={() => {closeModal()}}>Cancel</button>
-                            <button type="submit" className="btn btn-primary" form="modal-details">Register</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <DialogTitle  className="d-flex align-items-center justify-content-between bg-light border-bottom" >
+                Member Registration
+                <IconButton onClick={handleCloseModal}>
+                    <CloseIcon />
+                </IconButton>
+            </DialogTitle>
         );
     }
+
+
+    function renderDialog(): JSX.Element {
+
+        return (
+            <div>
+                <Dialog
+                    open={openModal}
+                    TransitionComponent={Transition}
+                    keepMounted
+                    maxWidth='xs'
+                >
+                    {renderDialogHeader()}
+                    <DialogContent>
+                        <form onSubmit={formik.handleSubmit}>
+                            <Grid container className="form-container">
+                                <Grid item sm={12} xs={12}>
+                                    <TextField id="firstname"
+                                        label="Firstname"
+                                        variant="outlined"
+                                        fullWidth
+                                        error={(formik.touched.firstname && formik.errors.firstname) ? Boolean(formik.errors.firstname) : false}
+                                        helperText={(formik.touched.firstname && formik.errors.firstname) ? formik.errors.firstname : null}
+                                        {...formik.getFieldProps('firstname')}
+                                    />
+                                </Grid>
+                                <Grid item sm={12} xs={12}>
+                                    <TextField id="lastname"
+                                        label="Lastname"
+                                        variant="outlined"
+                                        fullWidth
+                                        error={(formik.touched.lastname && formik.errors.lastname) ? Boolean(formik.errors.lastname) : false}
+                                        helperText={(formik.touched.lastname && formik.errors.lastname) ? formik.errors.lastname : null}
+                                        {...formik.getFieldProps('lastname')}
+                                    />
+                                </Grid>
+                                <Grid item sm={12} xs={12}>
+                                    <TextField id="email"
+                                        label="Email"
+                                        variant="outlined"
+                                        fullWidth
+                                        error={(formik.touched.email && formik.errors.email) ? Boolean(formik.errors.email) : false}
+                                        helperText={(formik.touched.email && formik.errors.email) ? formik.errors.email : null}
+                                        {...formik.getFieldProps('email')}
+                                    />
+                                </Grid>
+                                <Grid item sm={12} xs={12}>
+                                    <LocalizationProvider dateAdapter={AdapterDateFns} locale={localeDate.locale}>
+                                        <DatePicker
+                                            label="Date of birth"
+                                            mask={localeDate.mask}
+                                            value={formik.values.birthdate}
+                                            onChange={(date) => { formik.setFieldValue('birthdate', date) }}
+                                            renderInput={(params) => <TextField id="birthdate" {...params} fullWidth />}
+
+                                        />
+
+                                    </LocalizationProvider>
+                                </Grid>
+                                <Grid item sm={12} xs={12}>
+                                    <TextField
+                                        id="gender"
+                                        label="Gender"
+                                        select
+                                        fullWidth
+                                        error={ (formik.touched.gender && formik.errors.gender) ? Boolean(formik.errors.gender) : false}
+                                        helperText={(formik.touched.gender && formik.errors.gender) ? formik.errors.gender : null}
+                                        {...formik.getFieldProps('gender')}
+                                    >
+                                        <MenuItem value={0}>Male</MenuItem>
+                                        <MenuItem value={1}>Female</MenuItem>
+                                        <MenuItem value={2}>Other</MenuItem>
+                                    </TextField>
+                                </Grid>
+                            </Grid>
+                            <DialogActions className="bg-light border-top p-3">
+                                <Button className="close-button" variant="outlined" onClick={handleCloseModal}>Cancel</Button>
+                                <Button type="submit" color="primary" variant="contained">Register</Button>
+                            </DialogActions>
+                        </form>
+                    </DialogContent>
+                </Dialog>
+            </div>
+        );
+
+
+    }
+
 
     return (
         <React.Fragment>
             {renderMain()}
             {renderFloatingButton()}
-            {renderFormDialog()}
+            {renderDialog()}
         </React.Fragment>
     );
 }
